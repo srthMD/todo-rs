222095cd58ef36a19b8e8272b076540c5f4906a7
srth <srthro@proton.me>
2024-11-20 12:00:33 +0200

    refractor how entries are stylized a bit and change eq and ne impl

--- src/entry.rs
+++ src/entry.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Entry {
     pub name: String,
-    pub status: EntryStatus
+    pub status: EntryStatus,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Serialize, Deserialize, Debug)]
@@ -20,22 +20,28 @@ impl Entry {
     pub fn new(name: String) -> Entry {
         Entry {
             name,
-            status: EntryStatus::Incomplete
+            status: EntryStatus::Incomplete,
         }
     }
 
     pub fn to_styled(self) -> StyledObject<String> {
-        // where the fuck else am i supposed to put these
-        let incomplete_style: Style = Style::new();
-        let complete_style: Style = Style::new().green().bold().underlined();
-        let inprogress_style: Style = Style::new().yellow().blink();
-        let scrapped_style: Style = Style::new().strikethrough().dim();
-
         match self.status {
-            EntryStatus::Incomplete => { incomplete_style.apply_to(self.name) }
-            EntryStatus::InProgress => { inprogress_style.apply_to(self.name) }
-            EntryStatus::Scrapped => { scrapped_style.apply_to(self.name) }
-            EntryStatus::Completed => { complete_style.apply_to(self.name) }
+            EntryStatus::Incomplete => {
+                let incomplete_style: Style = Style::new();
+                incomplete_style.apply_to(self.name)
+            }
+            EntryStatus::InProgress => {
+                let inprogress_style: Style = Style::new().yellow().blink();
+                inprogress_style.apply_to(self.name)
+            }
+            EntryStatus::Scrapped => {
+                let scrapped_style: Style = Style::new().strikethrough().dim();
+                scrapped_style.apply_to(self.name)
+            }
+            EntryStatus::Completed => {
+                let complete_style: Style = Style::new().green().bold().underlined();
+                complete_style.apply_to(self.name)
+            }
         }
     }
 
@@ -46,10 +52,11 @@ impl Entry {
 
 impl PartialEq for Entry {
     fn eq(&self, other: &Self) -> bool {
-        self.name.eq(&other.name)
+        self.name.eq(&other.name) && self.status.eq(&other.status)
     }
 
     fn ne(&self, other: &Self) -> bool {
-        self.name.ne(&other.name)
+        self.name.ne(&other.name) && self.status.ne(&other.status)
     }
 }
+
